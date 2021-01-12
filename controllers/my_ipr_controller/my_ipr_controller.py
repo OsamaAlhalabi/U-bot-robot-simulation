cdaa69f9efc493fdf78601647ade3f6fefa5a344
Nitro963 <adnankattan9@gmail.com>
2021-01-12 15:41:35 +0200

    modified world

--- controllers/my_ipr_controller/my_ipr_controller.py
+++ controllers/my_ipr_controller/my_ipr_controller.py
@@ -66,51 +66,56 @@ def closeGripper():
   supervisor.getMotor('left_gripper').setPosition(-0.35);
   passiveWait(.5)
   
-def move2pos(arm, x, y, z, plus=True):
+def move2pos(arm, x, y, z):
     armPosition = arm.getPosition()
 
     tx = x - armPosition[0]
     ty = y - armPosition[1]
     tz = z - armPosition[2]
     
-    ikResults = armChain.inverse_kinematics([tx - 0.05, ty + 0.03, tz + 0.02])
-    for i in range(len(motors)):
-        if plus:
-            motors[i].setPosition(ikResults[i + 1])
-        else:
-            motors[i].setPosition(ikResults[i])
+    t = 1 if tz <= 0 else -1
     
+    ikResults = armChain.inverse_kinematics([tx - 0.05, ty + 0.015, tz + t * 0.023])
+    for i in range(len(motors)):
+        motors[i].setPosition(ikResults[i + 1])
+            
     passiveWait(4.0)
-
+        
 radar = supervisor.getRadar("radar")
 radar.enable(timeStep)
 
-
-for i in range(5):
-    targetName = f'CUBE{i}'
-    print(targetName)
-    # Get the arm and target nodes.
-    target = supervisor.getFromDef(targetName)
-    openGripper()
-    print('moveing')
-    move2pos(supervisor.getSelf(), *target.getPosition())
-    print("closing")
-    closeGripper()
-    target = supervisor.getFromDef('BASKET')
-    x, y, z = target.getPosition()
-    move2pos(supervisor.getSelf(), x, y + 0.3, z)
-    openGripper()
+# for i in range(5):
+    # targetName = f'CUBE{i}'
+    # print(targetName)
+    # target = supervisor.getFromDef(targetName)
+    # openGripper()
+    # print('moveing')
+    # move2pos(supervisor.getSelf(), *target.getPosition())
+    # print("closing")
+    # closeGripper()
+    # target = supervisor.getFromDef('BASKET')
+    # x, y, z = target.getPosition()
+    # move2pos(supervisor.getSelf(), x, y + 0.3, z)
+    # openGripper()
     
-# while supervisor.step(timeStep) != -1:
-    # targets = radar.getTargets()
-    # if targets:
-        # for target in targets:
-            # distance = target.distance
-            # print(distance)
-            # theta = target.azimuth
-            # x = distance * math.cos(theta - 1.57)
-            # y = 0.66
-            # z = distance * math.sin(theta - 1.57)
-        # break
-        
-# passiveWait(10)	
+while supervisor.step(timeStep) != -1:
+    targets = radar.getTargets()
+    if targets:
+        for target in targets:
+            distance = target.distance
+            print(distance)
+            theta = target.azimuth
+            x = distance * math.cos(theta)
+            y = 0.66
+            z = distance * math.sin(theta)
+            openGripper()
+            print('moveing')
+            move2pos(supervisor.getSelf(), x + 0.99, y, z)
+            print("closing")
+            closeGripper()
+            target = supervisor.getFromDef('BASKET')
+            x, y, z = target.getPosition()
+            move2pos(supervisor.getSelf(), x, y + 0.3, z)
+            openGripper()
+                
+passiveWait(10)	
